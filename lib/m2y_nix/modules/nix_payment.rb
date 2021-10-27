573d55afa04b97619f33ba216c31b9c5006b54e5
Guillaume <guillaume.zeldine@mobile2you.com.br>
2021-10-27 12:34:32 -0300

    created fees endpoint

--- lib/m2y_nix/modules/nix_payment.rb
+++ lib/m2y_nix/modules/nix_payment.rb
@@ -4,43 +4,18 @@ module M2yNix
       startModule(access_key)
     end
 
-    def transfer(params)
-      # body = {
-      #   type: String,
-      #   amount: Float,
-      #   description: String,
-      #   recipient: {
-      #     account: String,
-      #     name: String,
-      #     social_number: String,
-      #     branch: String,
-      #     bank: {
-      #       code: String,
-      #       name: String
-      #     },
-      #     account_type: String
-      #   }
-      # }
+    def fees
+      response = @request.get(@url + FEE_PATH)['results']
 
-      recipient = params[:recipient]
-      bank = params[:recipient][:bank]
-
-      body = {
-        "amount": params[:amount],
-        "description": params[:description], # Opcional
-        "recipient_account": recipient[:account],
-        "recipient_name": recipient[:name],
-        "recipient_social_number": recipient[:social_number],
-        "recipient_branch": recipient[:branch],
-        "recipient_account_type": recipient[:account_type],
-        "recipient_bank_code": bank[:code],
-        "recipient_bank_name": bank[:name]
-      }
-
-      if body[:type] == 'NIX'
-        response = @request.post(@url + TRANSFER_PATH + '/bank', body)
-      elsif body[:type] == 'BANK_TRANFER'
-        response = @request.post(@url + TRANSFER_PATH, body)
+      response.map do |fee|
+        {
+          transaction_type: fee['transaction_type'],
+          installment: fee['installment'],
+          time_to_receive: fee['time_to_receive'],
+          time_to_receive_type: fee['time_to_receive_type'],
+          fee_type: fee['fee_type'],
+          amount: fee['amount']
+        }
       end
     end
   end
