4640ae36739831241fbc7967ff4147024e7e1df9
gabriel vasconcelos <gavascdev@gmail.com>
2022-05-16 16:17:29 -0300

    feat(NIX-1009): adding contactless control

--- lib/m2y_nix/modules/nix_card.rb
+++ lib/m2y_nix/modules/nix_card.rb
@@ -4,42 +4,34 @@ module M2yNix
   class NixCard < NixModule
     def initialize(access_key)
       startModule(access_key)
+      @headers = {
+        'Authorization': access_key,
+        'Content-Type' => 'application/json'
+      }
     end
 
     def create(body)
-      @request.post("#{@url}#{CARD_PATH}/#{body[:card_type].downcase}", body.except!(:card_type))
+      @request.post("#{@url}#{CARD_PATH}/#{body[:card_type].downcase}", body.except!(:card_type), headers: @headers)
     end
 
     def get_virtual(access_key)
-      headers = {
-        'Authorization': access_key,
-        'Content-Type' => 'application/json'
-      }
-      HTTParty.get("#{@url}#{CARD_PATH}/virtual", headers: headers)
+      HTTParty.get("#{@url}#{CARD_PATH}/virtual", headers: @headers)
     end
 
     def activate(body, access_key)
-      headers = {
-        'Authorization': access_key,
-        'Content-Type' => 'application/json'
-      }
-      HTTParty.patch("#{@url}#{CARD_PATH}/activate", body: body, headers: headers)
+      HTTParty.patch("#{@url}#{CARD_PATH}/activate", body: body, headers: @headers)
+    end
+
+    def contactless_control(body)
+      HTTParty.patch("#{url}#{CARD_PATH}/contactless", body, headers: @headers)
     end
 
     def cancel(body, access_key)
-      headers = {
-        'Authorization': access_key,
-        'Content-Type' => 'application/json'
-      }
-      HTTParty.patch("#{@url}#{CARD_PATH}/cancel", body: body, headers: headers)
+      HTTParty.patch("#{@url}#{CARD_PATH}/cancel", body: body, headers: @headers)
     end
 
     def change_password(body, access_key)
-      headers = {
-        'Authorization': access_key,
-        'Content-Type' => 'application/json'
-      }
-      HTTParty.patch("#{@url}#{CARD_PATH}/password", body: body, headers: headers)
+      HTTParty.patch("#{@url}#{CARD_PATH}/password", body: body, headers: @headers)
     end
 
     def sensitive_info(body)
