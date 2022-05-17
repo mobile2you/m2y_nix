5168685c7292dd6be499abb61242c19b81d7b6d4
gabriel vasconcelos <gavascdev@gmail.com>
2022-05-17 17:47:00 -0300

    final

--- lib/m2y_nix/modules/nix_card.rb
+++ lib/m2y_nix/modules/nix_card.rb
@@ -4,34 +4,50 @@ module M2yNix
   class NixCard < NixModule
     def initialize(access_key)
       startModule(access_key)
-      @headers = {
-        'Authorization': access_key,
-        'Content-Type' => 'application/json'
-      }
     end
 
     def create(body)
-      @request.post("#{@url}#{CARD_PATH}/#{body[:card_type].downcase}", body.except!(:card_type), headers: @headers)
+      @request.post("#{@url}#{CARD_PATH}/#{body[:card_type].downcase}", body.except!(:card_type))
     end
 
     def get_virtual(access_key)
-      HTTParty.get("#{@url}#{CARD_PATH}/virtual", headers: @headers)
+      headers = {
+        'Authorization': access_key,
+        'Content-Type' => 'application/json'
+      }
+      HTTParty.get("#{@url}#{CARD_PATH}/virtual", headers: headers)
     end
 
     def activate(body, access_key)
-      HTTParty.patch("#{@url}#{CARD_PATH}/activate", body: body, headers: @headers)
+      headers = {
+        'Authorization': access_key,
+        'Content-Type' => 'application/json'
+      }
+      HTTParty.patch("#{@url}#{CARD_PATH}/activate", body: body, headers: headers)
     end
 
-    def contactless_control(body)
-      HTTParty.patch("#{@url}#{CARD_PATH}/contactless", body: body, headers: @headers)
+    def contactless_control(body, access_key)
+      headers = {
+        'Authorization': access_key,
+        'Content-Type' => 'application/json'
+      }
+      HTTParty.patch("#{@url}#{CARD_PATH}/contactless", body: body, headers: headers)
     end
 
     def cancel(body, access_key)
-      HTTParty.patch("#{@url}#{CARD_PATH}/cancel", body: body, headers: @headers)
+      headers = {
+        'Authorization': access_key,
+        'Content-Type' => 'application/json'
+      }
+      HTTParty.patch("#{@url}#{CARD_PATH}/cancel", body: body, headers: headers)
     end
 
     def change_password(body, access_key)
-      HTTParty.patch("#{@url}#{CARD_PATH}/password", body: body, headers: @headers)
+      headers = {
+        'Authorization': access_key,
+        'Content-Type' => 'application/json'
+      }
+      HTTParty.patch("#{@url}#{CARD_PATH}/password", body: body, headers: headers)
     end
 
     def sensitive_info(body)
